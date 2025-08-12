3c92d11f0e546c63182d733aec007d0ebf867d7f
stan <moon_saga@yahoo.com.tw>
2025-08-12 20:41:27 +0800

    test .vue for the before and after animate

---
+++ src/components/test.vue
@@ -0,0 +1,49 @@
+<template>
+  <motion.div
+    class="box"
+    :initial="{ opacity: 0 }"
+    :whileInView="{ opacity: 1 }"
+    @viewport-enter="startAnim = true"
+    @viewport-leave="startAnim = false"
+    :class="{ active: startAnim }"
+  />
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { motion } from "motion-v";
+
+const startAnim = ref(false);
+</script>
+
+<style scoped>
+.box {
+  position: relative;
+  width: 120px;
+  height: 120px;
+  background: lightblue;
+  opacity: 0;
+  /* Motion will animate this */
+}
+
+.box::before {
+  content: "";
+  position: absolute;
+  inset: 0;
+  background: red;
+}
+
+.box.active::before {
+  animation: pulse 2s ease;
+}
+
+@keyframes pulse {
+  from {
+    transform: scale(1);
+  }
+
+  to {
+    transform: scale(3);
+  }
+}
+</style>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,15 +1,24 @@
 <template>
-    <ProductList/>
-    <UserList/>
-  
+
+    <div class="kk">
+        <ProductList />
+        <UserList />
+        <test />
+
+
+    </div>
+
 </template>
 
 <script setup>
 import ProductList from '@/components/ProductList.vue';
 import UserList from '@/components/UserList.vue';
+import test from '@/components/test.vue';
 
 </script>
 
 <style>
-
+.kk {
+    height: 2000px;
+}
 </style>
